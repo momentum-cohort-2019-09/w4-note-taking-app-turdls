3e9439989a159c60289436049492396950a14a3d
Charles Wines <ccrecorder@gmail.com>
2019-10-02 21:37:38 -0400

    main function
    Co-authored-by: agizzi <alexandra.n.gizzi@gmail.com>

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 
-const uuid4 = require('uuid/v4');
+const uuidv4 = require('uuid/v4');
 let credentials = {
 	username : sessionStorage.getItem('username'),
 	password : sessionStorage.getItem('password')
@@ -29,18 +29,20 @@ function renderPage() {
 function main () {
     renderPage()
     // how to change login vs sign up
-const loginForm = document.querySelector('#diveIn')
-loginForm.addEventListener('submit', function (event) {
-    event.preventDefault()
-    const formData = new FormData(loginForm)
-    const username = formData.get('username')
-    const password = formData.get('password')
+    const loginForm = document.querySelector('#diveIn')
+    loginForm.addEventListener('submit', function (event) {
+        event.preventDefault()
+        const formData = new FormData(loginForm)
+        const username = formData.get('username')
+        const password = formData.get('password')
 
-    fetch('https://notes-api.glitch.me/api/users', {
+        fetch('https://notes-api.glitch.me/api/users', {
         headers: {
             'Authorization': 'Basic ' + btoa(`${username}:${password}`)
-        }
-    })
+            }
+        })
+    }
+
     .then(response => {
         if (response.ok) {
             credentials.username = username
@@ -49,7 +51,7 @@ loginForm.addEventListener('submit', function (event) {
             sessionStorage.setItem('password', password)
             renderPage()
         } else {
-            document.getElementById('login-error').innerText = 'TurdL says you smell like a sea anemone (must be valid username and password'
+            document.querySelector('#login-error').innerText = 'TurdL says you smell like a sea anemone (must be valid username and password)'
         }
     })
-	
+}
