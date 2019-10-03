415468e135da0ed622803e058cb3e63d58f10b3c
agizzi <alexandra.n.gizzi@gmail.com>
2019-10-03 17:45:15 -0400

    start JS

--- scriptTwo.js
+++ scriptTwo.js
@@ -1,30 +1,38 @@
-
-
 const turdLTrinkets = {
+	setCredentials: function(username, password) {
+		this.credentials = {
+			username: username,
+			password: password
+		};
+		sessionStorage.setItem('username', username);
+		sessionStorage.setItem('password', password);
+	},
 
-setCredentials: function (username, password) {
-    this.credentials = {
-        username: username,
-        password: password
-    }
-    sessionStorage.setItem('username', username)
-    sessionStorage.setItem('password', password)
-},
+	addAuthHeader: function(headers) {
+		if (!headers) {
+			headers = {};
+		}
 
-addAuthHeader: function (headers) {
-    if (!headers) { headers = {} }
+		return Object.assign({}, headers, {
+			Authorization: 'Basic ' + btoa(`${app.data.credentials.username}:${app.data.credentials.password}`)
+		});
+	},
 
-    return Object.assign({}, headers, {
-      'Authorization': 'Basic ' + btoa(`${app.data.credentials.username}:${app.data.credentials.password}`)
-    })
-  },
+	getTrinkets: function() {
+		return fetch('https://notes-api.glitch.me/api/notes', {
+			headers: this.addAuthHeader()
+		})
+			.then((response) => response.json())
+			.then((response) => {
+				if (response.ok) {
+					console.log(response);
+				}
+			});
+	}
+};
 
-getTrinkets: function (),
-
-
-
-addTrinket: function (???) {
-    const uuid = uuidv4
-    const newTrinket = { 'title': title, 'tags': tags, 'content': content, 'id': uuid }
-}
-}
+turdLTrinkets.getTrinkets();
+// 	addTrinket: function() {
+// 		const newTrinket = { title: title, tags: tags, content: content };
+// 	}
+// };
