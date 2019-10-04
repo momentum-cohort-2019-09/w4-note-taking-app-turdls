293a877972fce5fba8826aa97de1723e3979b69d
agizzi <alexandra.n.gizzi@gmail.com>
2019-10-03 20:37:48 -0400

    commit with rebecca
    Co-authored-by: ccrecorder <ccrecorder@gmail.com>

--- scriptTwo.js
+++ scriptTwo.js
@@ -1,4 +1,42 @@
+/* globals fetch btoa */
+
+let login = function(username, password) {
+	fetch('https://notes-api.glitch.me/api/users', {
+		headers: {
+			Authorization: 'Basic' + btoa('Chuck:password')
+		}
+	}).then((response) => {
+		// console.log(this.headers);
+		console.log(response);
+		if (response.ok) {
+			this.setCredentials(username, password);
+			this.render();
+		} else {
+			document.getElementById('login-error').innerText = 'That is not a valid username and password.';
+			// Double check the 'login-error' .. unsure if class exists
+		}
+	});
+};
+
+function main() {
+	// turdLTrinkets.render()
+	const diveIn = document.querySelector('#diveIn');
+	diveIn.addEventListener('submit', function(event) {
+		event.preventDefault();
+		const turdLUser = new FormData(diveIn);
+		const username = turdLUser.get('username');
+		const password = turdLUser.get('password');
+	});
+}
+
 const turdLTrinkets = {
+	credentials: {
+		username: sessionStorage.getItem('username'),
+		password: sessionStorage.getItem('password')
+	},
+
+	trinkets: [],
+
 	setCredentials: function(username, password) {
 		this.credentials = {
 			username: username,
@@ -14,11 +52,13 @@ const turdLTrinkets = {
 		}
 
 		return Object.assign({}, headers, {
-			Authorization: 'Basic ' + btoa(`${app.data.credentials.username}:${app.data.credentials.password}`)
+			Authorization:
+				'Basic ' + btoa(`${turdLTrinkets.credentials.username}:${turdLTrinkets.credentials.password}`)
 		});
 	},
 
 	getTrinkets: function() {
+		console.log(this.addAuthHeader());
 		return fetch('https://notes-api.glitch.me/api/notes', {
 			headers: this.addAuthHeader()
 		})
